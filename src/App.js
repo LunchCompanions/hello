d426431e2ea6dc836d284ddd0a410c10e7f3900f
Sarcasmonaut <patrik.kessler@outlook.de>
2020-05-26 22:03:17 +0200

    Setup repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './snax.svg';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MisterSnax - Gruppenbestellungen einfach koordinieren.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://chrome.google.com/webstore/detail/mistersnax/jmobiglepfamkdimbpcjjaomhinajijp?hl=de&gl=DE"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get the Extension for Chrome!
         </a>
       </header>
     </div>
