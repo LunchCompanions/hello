81c67802d917c9874e793fb6c1cd6d5cc33b39e3
Sarcasmonaut <patrik.kessler@outlook.de>
2020-05-26 22:29:25 +0200

    Updates

---
+++ precache-manifest.afeb842c227b8ecb60a1aac2ceff3aca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321d047a6688e70a21b1495c0afe3848",
+    "url": "/landing-page-react/index.html"
+  },
+  {
+    "revision": "0a3760cda2c8e872a012",
+    "url": "/landing-page-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a678f8c983ebbdad8bb",
+    "url": "/landing-page-react/static/js/2.1f276ab6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing-page-react/static/js/2.1f276ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3760cda2c8e872a012",
+    "url": "/landing-page-react/static/js/main.ea04b1d9.chunk.js"
+  },
+  {
+    "revision": "9e1123cccf31f6ee88e8",
+    "url": "/landing-page-react/static/js/runtime-main.230cdde1.js"
+  },
+  {
+    "revision": "cfae81994a6affc364ef2a95d178b697",
+    "url": "/landing-page-react/static/media/snax.cfae8199.svg"
+  }
+]);
