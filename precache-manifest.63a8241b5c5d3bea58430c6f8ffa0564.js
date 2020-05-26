14aecaca536c08418d19445f56e00ea18104b788
Sarcasmonaut <patrik.kessler@outlook.de>
2020-05-26 22:31:35 +0200

    Updates

---
+++ precache-manifest.63a8241b5c5d3bea58430c6f8ffa0564.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96252c6c90d70e442fa094202a5a9e47",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "088fa7934f523ba64b26",
+    "url": "/hello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a678f8c983ebbdad8bb",
+    "url": "/hello/static/js/2.1f276ab6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.1f276ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "088fa7934f523ba64b26",
+    "url": "/hello/static/js/main.058e7033.chunk.js"
+  },
+  {
+    "revision": "4374c5cb6b55e9b6685c",
+    "url": "/hello/static/js/runtime-main.02e4605a.js"
+  },
+  {
+    "revision": "cfae81994a6affc364ef2a95d178b697",
+    "url": "/hello/static/media/snax.cfae8199.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a38c678c313fac5541488b4efdc4937f.js"
+  "/hello/precache-manifest.63a8241b5c5d3bea58430c6f8ffa0564.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hello/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
